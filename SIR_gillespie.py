391760604205a1b3ce08752c3253aa663fc098ed
Ziad Albitar <ziad@Ziads-MacBook-Air.local>
2025-10-09 21:50:50 +0200

    SIR gillespie

---
+++ SIR_gillespie.py
@@ -0,0 +1,33 @@
+from gillespie import SSA
+import numpy as np
+import matplotlib.pyplot as plt
+
+X0 = [995, 5, 0]
+
+N = 1000
+beta = 0.3
+gamma = 1/7
+tspan = (0, 120)
+
+def prop(X, coeff):
+    #beta * I / N  * S
+    prop0 = coeff[0]*(X[1] / N) * X[0] # 1.4925
+    #gamma * I
+    prop1 = coeff[1] * X[1] #
+    return np.array([prop0, prop1])
+
+def stoch():
+    return np.array([[-1, 1, 0],
+                     [0, -1, 1]])
+
+
+coeff = (beta, gamma)
+
+t, X = SSA(prop, stoch, X0, tspan, coeff)
+
+plt.plot(t, X[:,0], label="S")
+plt.plot(t, X[:,1], label="I")
+plt.plot(t, X[:,2], label="R")
+plt.legend()
+plt.grid()
+plt.show()
--- gillespie.py
+++ gillespie.py
@@ -1,4 +1,3 @@
-
 #==================================
 # Gillespies algoritm
 # prop - propensities (1D numpy-array)
@@ -8,7 +7,6 @@
 # coeff - model parameters
 #==================================
 import numpy as np
-import random
 
 def SSA(prop, stoch, X0, tspan, coeff):
     # prop  - propensities
@@ -16,26 +14,26 @@ def SSA(prop, stoch, X0, tspan, coeff):
     # Initial state vector
     tvec = np.zeros(1)
     tvec[0] = tspan[0]
-    Xarr = np.zeros([1,len(X0)])
+    Xarr = np.zeros([1, len(X0)])
     Xarr[0,:] = X0
     t = tvec[0]
     X = X0
     sMat = stoch()
-    while t<tspan[1]:
-        r1, r2 = np.random.uniform(0,1, size=2)  # Find two random numbers on uniform distr.
-        re = prop(X,coeff)
+    while t < tspan[1]:
+        r1, r2 = np.random.uniform(0, 1, size=2)  # Find two random numbers on uniform distr.
+        re = prop(X, coeff)
         cre = np.cumsum(re)
         a0 = cre[-1]
         if a0 < 1e-12:
             break
 
-        tau = np.random.exponential(scale=1/a0) # Random number exponential distribution
-        cre=cre/a0
+        tau = np.random.exponential(scale=1 / a0) # Random number exponential distribution
+        cre = cre / a0
         r = 0
-        while cre[r]<r2:
-            r+=1
+        while cre[r] < r2:
+            r += 1
         
-        t+= tau
+        t += tau
         # if new time is larger than final time, skip last calculation
         if t > tspan[1]:
             break
